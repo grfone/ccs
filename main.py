012b865f2c1274a761d105dc96a0e6c3376cc4e1
grfone <126663988+grfone@users.noreply.github.com>
2023-12-12 13:19:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import pandas
 # Read the CSV file into a pandas DataFrame
 metlin = pandas.read_csv('mMetlin.csv')
 
-# Remove all rows with the molecule name 'Tm_XXX' where XXX are three numbers
-metlin_without_Tms = metlin[~metlin['Molecule Name'].str.contains("Tm_")]
-
 print("Average standard deviation between runs of the same molecule:")
-print(metlin_without_Tms[['CCS1', 'CCS2', 'CCS3']].std(axis=1, skipna=True).mean())
+print(metlin[['CCS1', 'CCS2', 'CCS3']].std(axis=1, skipna=True).mean())
 
 print("Average standard deviation between experiments of the same molecule with different adduct:")
-print(metlin_without_Tms.groupby('InChIKEY')['CCS_AVG'].std().mean())
+print(metlin.groupby('InChIKEY')['CCS_AVG'].std().mean())
